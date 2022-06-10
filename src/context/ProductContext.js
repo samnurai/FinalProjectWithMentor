b2a18d6a8857a22beace6f2dd328b567b2da2c06
miarunm <borkoshevan@mail.ru>
2022-06-10 16:18:27 +0600

    fixing

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 
 export const productContext = createContext();
 
-const API = "http://localhost:8000/products";
+const API = "https://shopp-project.herokuapp.com/api/products";
 
 const INIT_STATE = {
   products: [],
